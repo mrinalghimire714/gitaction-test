1f69d00a13afb9cbb0d1c7706565f9c1e27787b1
Mrinal <mrinal.ghimire714@gmail.com>
2026-09-08 19:46:46 +0545

    updated

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-print ("Hello World")
-print ("Ok")
+import requests
+ 
+def main():
+    r = requests.get("https://catfact.ninja/fact")
+    print("Cat Fact", r.json()["fact"])
+ 
+if __name__ == "__main__":
+    main()
